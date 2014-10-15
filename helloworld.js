bd9fe895e17def96f9a10f0c979a66506ac2af5d
wuhaifengdhu <wuhaifengdhu@163.com>
2014-10-15 21:49:14 +0800

    ssh test

--- helloworld.js
+++ helloworld.js
@@ -2,8 +2,7 @@ var http = require('http');
 
 http.createServer(function(req, res){
    res.writeHead(200, {'content-type': 'text/plain'});
-   res.end("Hello, World!\n");
-   res.end('Hello, World!\n');
+   res.end("This is wu haifeng's web station\n");
 }).listen(80);
 
 console.log('Server running on 80');
