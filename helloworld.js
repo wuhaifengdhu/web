bb5b992763cc10ea83f69a7a642127eb49ded654
wuhaifengdhu <wuhaifengdhu@163.com>
2014-10-15 21:36:25 +0800

    cancel ssh test

--- helloworld.js
+++ helloworld.js
@@ -4,6 +4,6 @@ http.createServer(function(req, res){
    res.writeHead(200, {'content-type': 'text/plain'});
    res.end("Hello, World!\n");
    res.end('Hello, World!\n');
-}).listen(800);
+}).listen(80);
 
-console.log('Server running on 800');
+console.log('Server running on 80');
