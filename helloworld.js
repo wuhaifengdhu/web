b7ab67cbf1174cde83411ae4f25d4ceb385db189
haifwu <haifwu@ebay.com>
2015-03-19 16:17:20 +0800

    basic edition for a server to supply materials

--- helloworld.js
+++
@@ -1,8 +0,0 @@
-var http = require('http');
-
-http.createServer(function(req, res){
-   res.writeHead(200, {'content-type': 'text/plain'});
-   res.end("This is wu haifeng's web station\n");
-}).listen(80);
-
-console.log('Server running on 80');
